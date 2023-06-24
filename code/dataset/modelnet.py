0fefeccaa1c9ee1076c50f0f23042dc8eed7d191
Kentechx <kd_shen@163.com>
2023-06-24 20:32:53 +0800

    update

--- code/dataset/modelnet.py
+++ code/dataset/modelnet.py
@@ -15,9 +15,8 @@ def download_modelnet40():
     if not os.path.exists(DATA_DIR):
         os.mkdir(DATA_DIR)
     if not os.path.exists(os.path.join(DATA_DIR, 'modelnet40_ply_hdf5_2048')):
-        www = 'https://shapenet.cs.stanford.edu/media/modelnet40_ply_hdf5_2048.zip'
-        zipfile = os.path.basename(www)
-        os.system('wget --no-check-certificate %s; unzip %s' % (www, zipfile))
+        zipfile = os.path.basename(url)
+        os.system('wget --no-check-certificate %s; unzip %s' % (url, zipfile))
         os.system('mv %s %s' % ('modelnet40_ply_hdf5_2048', DATA_DIR))
         os.system('rm %s' % (zipfile))
 
